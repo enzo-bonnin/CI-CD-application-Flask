699aa7a469ba0d5f2ffc81e46897fabc2a27a0b7
enzobonnin <enzo.bonnin@etu.univ-amu.fr>
2023-12-12 11:41:09 +0100

    feat: add tests and web folders

---
+++ web/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import json
+
+app= Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello from Flask !"
+
+app = Flask(__name__)
+@app.route('/plus_one')
+def plus_one():
+    x = int(request.args.get('x', 1))
+    return json.dumps({'x': x + 1})
+
+@app.route('/plus_two')
+def plus_two():
+    x = int(request.args.get('x', 1))
+    return json.dumps({'x': x + 2})
+
+@app.route('/square')
+def square():
+    x = int(request.args.get('x', 1))
+    return json.dumps({'x': x * x})
+     
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=5001)
+
